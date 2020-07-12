8687db35b52a1df7881b83543a9f8ea40138548d
Veera <pkondalu@gmail.com>
2020-07-12 14:56:41 +0530

    Adding exceptionally method

--- async/src/test/java/ThanApplyUsage.java
+++ async/src/test/java/ThanApplyUsage.java
@@ -5,14 +5,32 @@ import java.util.concurrent.CompletableFuture;
 public class ThanApplyUsage {
 
     @Test
-    public void BasicUsage(){
+    public void BasicUsage() {
         // Creating the Completable future with input 2
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> 2);
 
-        // thenAccept is executed once the code block from supplyAync is executed.
+        // thenAccept is executed once the code block from supplyAsync is executed.
         future.thenAccept(t -> {
             System.out.println(String.format("Received the input: %d", 2));
         });
+    }
+
+    @Test
+    public void ExceptionUseCase() throws RuntimeException {
+        // Creating the Completable future with exception in supply sync.
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            throw new RuntimeException("Exception in Async");
+        });
+
+        // here thenAccept won't be executed because of exception in supplyAsync.
+        // To handle exception we need to add exceptionally block
+        future.thenAccept(t -> {
+            System.out.println(String.format("Received the input: %d", 2));
+        }).exceptionally(t -> {
+            System.out.println(String.format("Exception received: " + t.getMessage()));
+            return  null; // exceptionally block excepts the return value.
+        });
+
     }
 
 }
---
+++ async/src/test/java/readme.md
@@ -0,0 +1,3 @@
+### Examples of CompletableFuture
+- **thenAccept** is used to accept the result from previous method in the chain. 
+- **exceptionally** - is used to handle the exceptions in the chain of methods.
