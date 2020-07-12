660b6ff04752e1e113588bf7bbc08f9ee69b9301
Veera <pkondalu@gmail.com>
2020-07-12 15:41:23 +0530

    Adding Async functions

--- async/src/test/java/ThanAcceptUsage.java
+++ async/src/test/java/ThanAcceptUsage.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
 public class ThanAcceptUsage {
 
@@ -55,14 +56,52 @@ public class ThanAcceptUsage {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> 2);
         future.thenApply(n -> {
             System.out.println(String.format("Received the input : %d", n));
-            return n*n;
+            return n * n;
         }).thenAccept(n -> {
             throw new RuntimeException("Exception");
-        }).thenAccept(n ->{ /*This method won't be executed because of the exception in previous stage.*/
-            System.out.println("Received:"+ n);
+        }).thenAccept(n -> { /*This method won't be executed because of the exception in previous stage.*/
+            System.out.println("Received:" + n);
         }).exceptionally(t -> { // This will be executed because of the unhandled exception in previous stage.
             System.out.println("Exception Occurred");
             return null;
         });
+    }
+
+    @Test
+    public void UnderstandThreads() {
+        DisplayThread(); // Display main thread.
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            DisplayThread(); // It will be ForkJoinPool.commonPool-worker
+            return 2;
+        });
+        future.thenAccept(t -> {
+            DisplayThread(); // main.
+            System.out.println(String.format("Received the input: %d", 2));
+        });
+    }
+
+    @Test
+    public void UsingOwnThreadPool() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(4);
+
+        DisplayThread(); // Display main thread.
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            DisplayThread(); // It will be ForkJoinPool.commonPool-worker
+            return 2;
+        }, forkJoinPool);
+
+        // Async functions take the Fork join Pool, if you want run the particular stage in your thread pool, you can use Async functions.
+        future.thenApplyAsync(t -> {
+            DisplayThread();
+            return t * t;
+        }, forkJoinPool).thenAcceptAsync(t -> {
+            DisplayThread(); // main.
+            System.out.println(String.format("Received the input: %d", 2));
+        }, forkJoinPool);
+    }
+
+
+    private void DisplayThread() {
+        System.out.println(Thread.currentThread().getName());
     }
 }
